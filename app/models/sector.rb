ba44c14814d494dd3e9763255c23d73c5d25611b
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-05-09 17:19:58 -0300

    validations sector

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,2 +1,7 @@
 class Sector < ApplicationRecord
+
+  validates :name,
+            presence: true,
+            length: { minimum: 5, maximum: 50 }
+            uniqueness: { case_sensitive: false }
 end
