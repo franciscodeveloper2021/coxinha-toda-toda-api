81a037da89218f02401e4bea7ec298e6b686ede3
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-05-09 16:54:15 -0300

    removing migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_08_170126) do
+ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "sectors", force: :cascade do |t|
-    t.string "name", limit: 50, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.check_constraint "length(name::text) >= 5", name: "min_length_name"
-  end
 
 end
