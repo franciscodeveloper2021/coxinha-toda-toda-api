b5532ee39b74984023a2a455c2e1a45729a31694
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-05-10 03:17:42 -0300

    first test case passing uhuul

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -2,6 +2,6 @@ class Sector < ApplicationRecord
 
   validates :name,
             presence: true,
-            length: { minimum: 5, maximum: 50 }
+            length: { minimum: 5, maximum: 50 },
             uniqueness: { case_sensitive: false }
 end
--- spec/models/sector_spec.rb
+++ spec/models/sector_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Sector, type: :model do
-   
+  let(:sector) { build(:sector) }
+
+  describe "validates" do
+    context "when sector's name is not present" do
+      it "raises an ActiveRecord error when sector's name is blank" do
+        sector.name = nil
+        sector.valid?
+
+        expect(sector.errors.full_messages).to include(I18n.t("activerecord.errors.full_messages.blank", attribute: Sector.human_attribute_name(:name)))
+      end
+    end
+  end
 end
