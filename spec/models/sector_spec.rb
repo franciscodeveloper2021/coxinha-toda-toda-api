b3f5ee6e2f89e73e988890b01d96522a4b276ef2
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-05-11 03:03:06 -0300

    wip on rspecs

--- spec/models/sector_spec.rb
+++ spec/models/sector_spec.rb
@@ -7,48 +7,61 @@ RSpec.describe Sector, type: :model do
   describe "validates" do
     context "when name attribute is not valid" do
 
-      context "when sector's name is blank" do
-        it "raises an ActiveModel blank error" do
-          sector.name = nil
-          sector.valid?
+      context "when sector's name is not present" do
+        context "with nil string value" do
+          it "raises an ActiveModel blank error" do
+            sector.name = nil
+            sector.valid?
 
-          expect(sector.errors.full_messages)
-            .to include(
-              I18n.t("activerecord.errors.full_messages.blank", attribute: attribute_name)
-            )
+            expect(sector.errors.full_messages)
+              .to include(
+                I18n.t("activerecord.errors.full_messages.blank", attribute: attribute_name)
+              )
+          end
+        end
+        context "with empty string value" do
+          it "raises an ActiveModel blank error" do
+            sector.name = ""
+            sector.valid?
+
+            expect(sector.errors.full_messages)
+              .to include(
+                I18n.t("activerecord.errors.full_messages.blank", attribute: attribute_name)
+              )
+          end
         end
       end
 
-      context "when sectors's name is too short" do
-        it "raises an ActiveModel too short error" do
-          below_minimum_length_name = Sector::MINIMUM_NAME_LENGTH - 1
+      context "when sector's name length is not valid" do
+        context "with too short name" do
+          it "raises an ActiveModel too short error" do
+            below_minimum_length_name = Sector::MINIMUM_NAME_LENGTH - 1
 
-          sector.name = sector.name.slice!(below_minimum_length_name..)
-          sector.valid?
+            sector.name = sector.name.slice!(below_minimum_length_name..)
+            sector.valid?
 
-          expect(sector.errors.full_messages)
-            .to include(
-              I18n.t("activerecord.errors.full_messages.too_short", attribute: attribute_name, count: Sector::MINIMUM_NAME_LENGTH)
-            )
+            expect(sector.errors.full_messages)
+              .to include(
+                I18n.t("activerecord.errors.full_messages.too_short", attribute: attribute_name, count: Sector::MINIMUM_NAME_LENGTH)
+              )
+          end
         end
-      end
+        context "with too long name" do
+          it "raises an ActiveModel too long error" do
+            name_above_maximum_length = sector.name.slice!(1..) * (Sector::MAXIMUM_NAME_LENGTH + 1)
 
-      context "when sector's name is too long" do
-        it "raises an ActiveModel too long error" do
-          name_above_maximum_length = sector.name.slice!(1..) * (Sector::MAXIMUM_NAME_LENGTH + 1)
+            sector.name = name_above_maximum_length
+            sector.valid?
 
-          sector.name = name_above_maximum_length
-          sector.valid?
-
-          expect(sector.errors.full_messages)
-            .to include(
-              I18n.t("activerecord.errors.full_messages.too_long", attribute: attribute_name, count: Sector::MAXIMUM_NAME_LENGTH)
-            )
+            expect(sector.errors.full_messages)
+              .to include(
+                I18n.t("activerecord.errors.full_messages.too_long", attribute: attribute_name, count: Sector::MAXIMUM_NAME_LENGTH)
+              )
+          end
         end
       end
 
       context "when sector's name has been already taken" do
-
         context "with the same string case" do
           let(:valid_sector) { create(:sector, name: "Bebidas") }
           let(:invalid_sector) { build(:sector, name: "Bebidas") }
@@ -62,7 +75,6 @@ RSpec.describe Sector, type: :model do
               )
           end
         end
-
         context "with different string case" do
           let(:valid_sector) { create(:sector, name: "Bebidas") }
           let(:invalid_sector) { build(:sector, name: "bebidas") }
