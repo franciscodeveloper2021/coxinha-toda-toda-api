145b3655e9f74825b7edce1a3633ab32311ffebc
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-05-12 04:45:16 -0300

    refactoring tests

--- spec/models/sector_spec.rb
+++ spec/models/sector_spec.rb
@@ -4,12 +4,36 @@ RSpec.describe Sector, type: :model do
   let!(:sector) { build(:sector) }
   let(:attribute_name) { Sector.human_attribute_name(:name) }
 
+  describe "sorbet type checking" do
+    context "with invalid attributes type" do
+      context "when name attribute is not a String" do
+        it "raises a TypeError" do
+          sector.name = 123
+
+          expect { T.assert_type!(sector.name, String) }.to raise_error(TypeError)
+        end
+      end
+    end
+
+    context "with valid attributes type" do
+      context "when name attribute is a String" do
+        it "ensures type checking for the name attribute" do
+          T.assert_type!(sector.name, String)
+        end
+
+        it "does not raise a type error" do
+          expect { T.assert_type!(sector.name, String) }.not_to raise_error
+        end
+      end
+    end
+  end
+
   describe "validates" do
     context "with invalid attributes" do
       context "when name attribute is invalid" do
         context "when sector's name is not present" do
           context "with empty string value" do
-            it "raises an ActiveModel blank error" do
+            it "receives an ActiveModel blank error" do
               sector.name = ""
               sector.valid?
 
@@ -23,7 +47,7 @@ RSpec.describe Sector, type: :model do
 
         context "when sector's name length is not valid" do
           context "with too short name" do
-            it "raises an ActiveModel too short error" do
+            it "receives an ActiveModel too short error" do
               below_minimum_length_name = Sector::MINIMUM_NAME_LENGTH - 1
 
               sector.name = sector.name.slice!(below_minimum_length_name..)
@@ -36,7 +60,7 @@ RSpec.describe Sector, type: :model do
             end
           end
           context "with too long name" do
-            it "raises an ActiveModel too long error" do
+            it "receives an ActiveModel too long error" do
               name_above_maximum_length = sector.name.slice!(1..) * (Sector::MAXIMUM_NAME_LENGTH + 1)
 
               sector.name = name_above_maximum_length
@@ -55,7 +79,7 @@ RSpec.describe Sector, type: :model do
             let(:valid_sector) { create(:sector, name: "Bebidas") }
             let(:invalid_sector) {  build(:sector, name: "Bebidas") }
 
-            it "raises an ActiveModel taken error" do
+            it "receives an ActiveModel taken error" do
               invalid_sector.valid?
 
               expect(invalid_sector.errors.full_messages)
@@ -68,7 +92,7 @@ RSpec.describe Sector, type: :model do
             let(:valid_sector) { create(:sector, name: "Bebidas") }
             let(:invalid_sector) { build(:sector, name: "bebidas") }
 
-            it "raises an ActiveModel taken error" do
+            it "receives an ActiveModel taken error" do
               invalid_sector.valid?
 
               expect(invalid_sector.errors.full_messages)
@@ -85,30 +109,6 @@ RSpec.describe Sector, type: :model do
       context "when name attribute is valid" do
         it "is allowed to be persisted on the database" do
           expect(sector.valid?).to be(true)
-        end
-      end
-    end
-  end
-
-  describe "sorbet type checking" do
-    context "with invalid type" do
-      context "when name attribute is not a String" do
-        it "raises a TypeError" do
-          sector.name = 123
-
-          expect { T.assert_type!(sector.name, String) }.to raise_error(TypeError)
-        end
-      end
-    end
-
-    context "with valid type" do
-      context "when name attribute is a String" do
-        it "ensures type checking for the name attribute" do
-          T.assert_type!(sector.name, String)
-        end
-
-        it "does not raise a type error" do
-          expect { T.assert_type!(sector.name, String) }.not_to raise_error
         end
       end
     end
