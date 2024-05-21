af032612bf9b17cb30d3b3548b40e1d6bffe0776
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-05-21 18:05:42 -0300

    wip

--- spec/request/sectors_spec.rb
+++ spec/request/sectors_spec.rb
@@ -2,13 +2,12 @@
 require "rails_helper"
 
 RSpec.describe "/sectors", type: :request do
-  let(:index_service_instance) { UseCases::Sector::IndexSectorsService.new }
+  let(:index_service_class) { UseCases::Sector::IndexSectorsService }
+  let(:subject)  { SectorsController.new }
 
   describe "initialize" do
     it "initializes the IndexSectorsService" do
-      controller = SectorsController.new
-
-      expect(controller.instance_variable_get(:@index_service)).to eq(index_service_instance)
+      expect(subject.instance_variable_get(:@index_service)).to be_a(index_service_class)
     end
   end
 end
