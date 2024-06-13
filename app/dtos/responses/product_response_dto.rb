1e166a6d6cbba43eea95edc8ce229965643cd6d6
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-06-13 05:14:43 -0300

    response DTO

---
+++ app/dtos/responses/product_response_dto.rb
@@ -0,0 +1,34 @@
+# typed: strict
+module Responses
+  class ProductResponseDto
+    extend T::Sig
+
+    sig { returns(Integer) }
+    attr_reader :id
+
+    sig { returns(String) }
+    attr_reader :name
+
+    sig { returns(String) }
+    attr_reader :description
+
+    sig { returns(Float) }
+    attr_reader :price
+
+    sig { returns(T::Boolean) }
+    attr_reader :available
+
+    sig { returns(Integer) }
+    attr_reader :sector_id
+
+    sig { params(id: Integer, name: String, description: String, price: Float, available: T::Boolean, sector_id: Integer).void }
+    def initialize(id:, name:, description:, price:, available:, sector_id:)
+      @id = T.let(id, Integer)
+      @name = T.let(name.strip, String)
+      @description = T.let(description.strip, String)
+      @price = T.let(price, Float)
+      @available = T.let(available, T::Boolean)
+      @sector_id = T.let(sector_id, Integer)
+    end
+  end
+end
--- app/repositories/product_repository.rb
+++ app/repositories/product_repository.rb
@@ -5,13 +5,13 @@ class ProductRepository < Interfaces::RepositoryInterface
   sig { void }
   def initialize
     @products_dtos = T.let([], T::Array[Responses::ProductResponseDto])
-    initialize_products_dtos
+    
   end
 
   private
 
   sig { returns(T::Array[Responses::ProductResponseDto]) }
   def initialize_products_dtos
-    @products_dtos = Product.order(:id).map { |product| Responses::ProductResponseDto.new }
+   
   end
 end
