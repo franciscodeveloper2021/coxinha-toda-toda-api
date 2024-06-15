211c4f780dd3e45a96cf10aff8d97adccbdc5de4
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-06-15 04:15:06 -0300

    refactored dtos

--- app/dtos/responses/sector_response_dto.rb
+++ app/dtos/responses/sector_response_dto.rb
@@ -1,18 +1,8 @@
-# typed:strict
+# typed: strict
 module Responses
-  class SectorResponseDto
-    extend T::Sig
-
-    sig { returns(Integer) }
-    attr_reader :id
-
-    sig { returns(String) }
-    attr_reader :name
-
-    sig { params(id: Integer, name: String).void }
-    def initialize(id:, name:)
-      @id = T.let(id, Integer)
-      @name = T.let(name.strip, String)
-    end
+  class SectorResponseDto < T::Struct
+    const :id, Integer
+    const :name, String
   end
 end
+
