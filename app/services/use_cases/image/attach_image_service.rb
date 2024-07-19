42e12a59760a612b9101f95c1b2e1ec8eaf5513e
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-07-19 02:41:22 -0300

    AttachImageService

--- app/services/use_cases/image/attach_image_service.rb
+++ app/services/use_cases/image/attach_image_service.rb
@@ -8,6 +8,11 @@ module UseCases
       def initialize(image_repository:)
         @repository = image_repository
       end
+
+      sig { params(image_request_dto: Requests::ImageRequestDto).void }
+      def call(image_request_dto:)
+        @repository.attach_image(image_request_dto: image_request_dto)
+      end
     end
   end
 end
