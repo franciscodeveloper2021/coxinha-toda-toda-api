385b2043a2877538cb30a2d83cb32c4002c7f63b
franciscodeveloper2021 <franciscosantanaalvesleite@gmail.com>
2024-07-25 02:57:43 -0300

    # Silence deprecation warnings ActiveSupport::Deprecation.behavior = :silence

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,6 +17,9 @@ require 'rspec/rails'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# Silence deprecation warnings
+ActiveSupport::Deprecation.behavior = :silence
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
